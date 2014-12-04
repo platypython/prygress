2c722feb274e6690868677241fd92b4991e2c7c4
Lucretiel <Lucretiel@users.noreply.github.com>
2014-12-04 15:16:18 -0500

    Various simplifications
    Localized everything- no more globals.
    Simplified try/finally block

--- prygress/prygress.py
+++ prygress/prygress.py
@@ -2,32 +2,24 @@ import sys
 import time
 import threading
 
-class ProgressBar(threading.Thread):
-    """Draw the dots, bar, or whatever"""
-    def run(self):
-        while not stop:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            time.sleep(0.2)
-
-        print '\b\b finished.',
-
-
 def progress(function):
     """Shows a progress bar while a function runs."""
     def wrap_function(*args):
-        global stop
         stop = False
-        p = ProgressBar()
-        p.start()
 
+        def progress_bar():
+            while not stop:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                time.sleep(0.2)
+            print '\b\b finished.',
+        
         try:
-            ran = function(*args)
-        except:
-            raise
+            p = threading.Thread(target=progress_bar)
+            p.start()
+            return function(*args)
         finally:
             stop = True
-
-        return ran
+            # p.join()
 
     return wrap_function
